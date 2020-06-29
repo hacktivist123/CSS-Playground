ff5915291503ff23e1165462c7c18c208c1d41b1
hacktivist123 <akintayoshedrack@gmail.com>
2020-06-29 02:14:50 +0100

    finished adding all needed component files

--- natours/src/App.js
+++ natours/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
-// import Button from './components/Button';
-// import Nav from './components/Nav';
-
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-
     </div>
   );
 }
